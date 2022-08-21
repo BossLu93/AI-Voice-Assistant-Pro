2e3eaf52c7faaf46a6e4f93facb977d54979b78d
BossLu93 <117741612+BossLu93@users.noreply.github.com>
2022-08-21 06:22:23 +0000

    version 0.4.6

--- Plugins/browsing_functionalities.py
+++ Plugins/browsing_functionalities.py
@@ -8,4 +8,7 @@ import websites
 
 def googleSearch(query):
 	if 'image' in query:
-		query
+		query += "&tbm=isch"
+	query = query.replace('images', '')
+	query = query.replace('image', '')
+	query = query
