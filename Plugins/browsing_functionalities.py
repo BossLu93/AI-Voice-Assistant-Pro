130ba44518a7cb292e38dbfcac4f1ccf1cd5bcb9
BossLu93 <117741612+BossLu93@users.noreply.github.com>
2022-09-11 10:03:48 +0000

    delete folder

--- Plugins/browsing_functionalities.py
+++ Plugins/browsing_functionalities.py
@@ -45,4 +45,9 @@ def get_speedtest():
 	try:
 		internet = speedtest.Speedtest()
 		speed = f"Your network's Download Speed is {round(internet.download() / 8388608, 2)}MBps\n" \
-			   f"Your network's Upload Speed is {round(internet.upload() / 8388608, 2
+			   f"Your network's Upload Speed is {round(internet.upload() / 8388608, 2)}MBps"
+		return speed
+	except (speedtest.SpeedtestException, KeyboardInterrupt) as e:
+		return
+
+def tell_
