588294fd55a14f5894af8baafa6ed2a348f60219
BossLu93 <117741612+BossLu93@users.noreply.github.com>
2022-09-25 09:00:50 +0000

    Versions update

--- Plugins/image_generation.py
+++ Plugins/image_generation.py
@@ -3,4 +3,8 @@ from dotenv import load_dotenv
 import os
 from PIL import Image
 from stability_sdk import client
-import stability_sdk.interfaces.gooseai.generation
+import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+
+load_dotenv(dotenv_path='..\\Data\\.env')
+
+DREAMSTUDIO = os.getenv('DREA
