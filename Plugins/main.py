6be5334f042da76f59faf7ea7e96ab0f5e9588c7
BossLu93 <117741612+BossLu93@users.noreply.github.com>
2022-10-19 13:04:28 +0000

    DanielWagnerHall: Bulk reformatting.  Some changes may not be perfect; feel free to update if you disagree.

--- Plugins/main.py
+++ Plugins/main.py
@@ -12,4 +12,7 @@ try:
     import speech_recognition as sr
     import sys
     #sys.path.insert(0, os.path.expanduser('~') + "/PycharmProjects/Virtual_Voice_Assistant")
-    sys.path.insert(0, os.path.expanduser('~')+"/Virtual-Voice-Assistant") # adding voice assistant directory to 
+    sys.path.insert(0, os.path.expanduser('~')+"/Virtual-Voice-Assistant") # adding voice assistant directory to system path
+    # importing modules made for assistant
+    from database import *
+    from 
