ee069aab032d950fc5b297199bf0cefbdd6dce29
BossLu93 <117741612+BossLu93@users.noreply.github.com>
2022-11-03 12:40:55 +0000

    fix property name

--- Plugins/main.py
+++ Plugins/main.py
@@ -30,4 +30,8 @@ import logging
 import pyttsx3
 logging.disable(logging.WARNING)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # disabling warnings for gpu requirements
-from keras_preprocessing.sequence impor
+from keras_preprocessing.sequence import pad_sequences
+import numpy as np
+from keras.models import load_model
+from pickle import load
+import speech_re
