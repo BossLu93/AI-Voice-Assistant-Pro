6a6c4e25871f0dd703c0727456a7b2b44d1c1b09
BossLu93 <117741612+BossLu93@users.noreply.github.com>
2023-03-09 06:37:06 +0000

    changelog tweak

--- Plugins/model_training.py
+++ Plugins/model_training.py
@@ -5,4 +5,6 @@ import logging
 logging.disable(logging.WARNING)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from keras.preprocessing.text import Tokenizer
-from keras_
+from keras_preprocessing.sequence import pad_sequences
+from sklearn.preprocessing import LabelEncoder
+fro
