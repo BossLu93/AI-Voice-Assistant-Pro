6cd0fb808a3138706868f4e8e4e25a6cefed42e8
BossLu93 <117741612+BossLu93@users.noreply.github.com>
2023-03-17 16:29:38 +0000

    Coding style fix

--- Plugins/model_training.py
+++ Plugins/model_training.py
@@ -8,4 +8,9 @@ from keras.preprocessing.text import Tokenizer
 from keras_preprocessing.sequence import pad_sequences
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, Embed
+from tensorflow.python.keras.layers import Dense, Embedding, GlobalAveragePooling1D
+
+with open('..\\Data\\intents.json') as file:
+    data = json.load(file)
+
+tr
