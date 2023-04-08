d1ed5272bde96adeb07a921f8d495361552fc9d5
BossLu93 <117741612+BossLu93@users.noreply.github.com>
2023-04-08 12:46:20 +0000

    Added navbar

--- Plugins/system_operations.py
+++ Plugins/system_operations.py
@@ -22,3 +22,111 @@ class SystemTasks:
         self.keyboard.press('a')
         self.keyboard.release('a')
         self.keyboard.release(Key.ctrl)
+
+    def hitEnter(self):
+        self.keyboard.press(Key.enter)
+        self.keyboard.release(Key.enter)
+
+    def delete(self):
+        self.select()
+        self.keyboard.press(Key.backspace)
+        self.keyboard.release(Key.backspace)
+
+    def copy(self):
+        self.select()
+        self.keyboard.press(Key.ctrl)
+        self.keyboard.press('c')
+        self.keyboard.release('c')
+        self.keyboard.release(Key.ctrl)
+
+    def paste(self):
+        self.keyboard.press(Key.ctrl)
+        self.keyboard.press('v')
+        self.keyboard.release('v')
+        self.keyboard.release(Key.ctrl)
+
+    def new_file(self):
+        self.keyboard.press(Key.ctrl)
+        self.keyboard.press('n')
+        self.keyboard.release('n')
+        self.keyboard.release(Key.ctrl)
+
+    def save(self, name):
+        self.keyboard.press(Key.ctrl)
+        self.keyboard.press('s')
+        self.keyboard.release('s')
+        self.keyboard.release(Key.ctrl)
+        time.sleep(0.2)
+        self.write(name)
+        self.hitEnter()
+
+
+class TabOpt:
+    def __init__(self):
+        self.keyboard = Controller()
+
+    def switchTab(self):
+        self.keyboard.press(Key.ctrl)
+        self.keyboard.press(Key.tab)
+        self.keyboard.release(Key.tab)
+        self.keyboard.release(Key.ctrl)
+
+    def closeTab(self):
+        self.keyboard.press(Key.ctrl)
+        self.keyboard.press('w')
+        self.keyboard.release('w')
+        self.keyboard.release(Key.ctrl)
+
+    def newTab(self):
+        self.keyboard.press(Key.ctrl)
+        self.keyboard.press('t')
+        self.keyboard.release('t')
+        self.keyboard.release(Key.ctrl)
+
+
+class WindowOpt:
+    def __init__(self):
+        self.keyboard = Controller()
+
+    def closeWindow(self):
+        self.keyboard.press(Key.alt_l)
+        self.keyboard.press(Key.f4)
+        self.keyboard.release(Key.f4)
+        self.keyboard.release(Key.alt_l)
+
+    def minimizeWindow(self):
+        for i in range(2):
+            self.keyboard.press(Key.cmd)
+            self.keyboard.press(Key.down)
+            self.keyboard.release(Key.down)
+            self.keyboard.release(Key.cmd)
+            time.sleep(0.05)
+
+    def maximizeWindow(self):
+        self.keyboard.press(Key.cmd)
+        self.keyboard.press(Key.up)
+        self.keyboard.release(Key.up)
+        self.keyboard.release(Key.cmd)
+
+    def switchWindow(self):
+        self.keyboard.press(Key.alt_l)
+        self.keyboard.press(Key.tab)
+        self.keyboard.release(Key.tab)
+        self.keyboard.release(Key.alt_l)
+
+    def Screen_Shot(self):
+        im = ImageGrab.grab()
+        im.save(f'../Data/Screenshots/ss_{randint(1, 100)}.jpg')
+
+
+def systemInfo():
+    c = wmi.WMI()
+    my_system_1 = c.Win32_LogicalDisk()[0]
+    my_system_2 = c.Win32_ComputerSystem()[0]
+    info = f"Total Disk Space: {round(int(my_system_1.Size)/(1024**3),2)} GB\n" \
+           f"Free Disk Space: {round(int(my_system_1.Freespace)/(1024**3),2)} GB\n" \
+           f"Manufacturer: {my_system_2.Manufacturer}\n" \
+           f"Model: {my_system_2. Model}\n" \
+           f"Owner: {my_system_2.PrimaryOwnerName}\n" \
+           f"Number of Processors: {psutil.cpu_count()}\n" \
+           f"System Type: {my_system_2.SystemType}"
